5c749f1981eccc8822ee13af3c4143c7c31c2c4d
haris ali baig <h.of.harris@gmail.com>
2022-12-29 13:20:40 +0200

    first commit

---
+++ Bejewel Clone/Assets/Scripts/IPiece.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shoelace.Piece
+{
+    public interface IPiece
+    {
+
+    }
+}
---
+++ Bejewel Clone/Assets/Scripts/Piece.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shoelace.Piece
+{
+    public class Piece : MonoBehaviour,IPiece
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
